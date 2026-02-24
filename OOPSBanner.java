6063316ed85d6074698ab333e65a6a3ee4982040
ky5697 -007 <ky5697@srmist.edu.in>
2026-02-24 08:26:34 +0530

    Codded UC6 to Console

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -2,20 +2,54 @@ public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        String[] banner = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-            String.join(" ", "  *****  ", "  *****  ", " ******  ", " ****** "),
-            String.join(" ", " *     * ", " *     * ", " *     * ", " *      "),
-            String.join(" ", "*       *", "*       *", " *     * ", " *      "),
-            String.join(" ", "*       *", "*       *", " ******  ", " ****** "),
-            String.join(" ", "*       *", "*       *", " *       ", "      * "),
-            String.join(" ", " *     * ", " *     * ", " *       ", " *    * "),
-            String.join(" ", "  *****  ", "  *****  ", " *       ", "  ****  ")
+        String[] banner = new String[7];
 
-        };
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", o[i], o[i], p[i], s[i]);
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    public static String[] getOPattern() {
+        return new String[] {
+            "  *****  ",
+            " *     * ",
+            "*       *",
+            "*       *",
+            "*       *",
+            " *     * ",
+            "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[] {
+            " ******  ",
+            " *     * ",
+            " *     * ",
+            " ******  ",
+            " *       ",
+            " *       ",
+            " *       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[] {
+            "  *****  ",
+            " *     * ",
+            " *       ",
+            "  *****  ",
+            "       * ",
+            " *     * ",
+            "  *****  "
+        };
     }
 }
